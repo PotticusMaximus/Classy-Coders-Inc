9752fc40effc2a0889ede7800f210e307127be2a
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-25 11:53:48 +0100

    All tests passed

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,14 +1,26 @@
 class Employees {
   #salary;
   #isHired;
+  static #allEmployees = [];
   constructor(name, position, salary) {
     this.name = name;
     this.#salary = salary;
     this.position = position;
     this.#isHired = true;
+    Employees.#allEmployees.push(this);
   }
   getSalary() {
     return this.#salary;
+  }
+  static getEmployees() {
+    return Employees.#allEmployees;
+  }
+  static getTotalPayroll() {
+    let count = 0;
+    for (let i = 0; i < Employees.#allEmployees.length; i++) {
+      count += Employees.#allEmployees[i].#salary;
+    }
+    return count;
   }
   setSalary(amount) {
     this.#salary = amount;
