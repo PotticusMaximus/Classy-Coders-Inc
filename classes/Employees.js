02ca997949e83a2aa8c499ae2aa0fb1adbe6425c
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-25 10:52:35 +0100

    employee tests passed

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,32 @@
 class Employees {
-
-
-
-    
+  #salary;
+  #isHired;
+  constructor(name, position, salary) {
+    this.name = name;
+    this.#salary = salary;
+    this.position = position;
+    this.#isHired = true;
+  }
+  getSalary() {
+    return this.#salary;
+  }
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+  getStatus() {
+    return this.#isHired;
+  }
+  setStatus(command) {
+    if (command !== "hire" && command !== "fire") {
+      throw new Error("Command must be either hire or fire");
+    } else if (command == "hire") {
+      this.#isHired = true;
+    } else if (command == "fire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
