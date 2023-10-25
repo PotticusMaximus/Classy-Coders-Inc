a241d992f5d5a20254d16d0fb3220f06544d518b
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-25 11:06:26 +0100

    Manager tests passed

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,27 @@
-class Manager {
+const { Employees } = require("./Employees");
 
-
-
-    
+class Manager extends Employees {
+  #employeesManaged;
+  constructor(name, position, salary, department, employeesManaged) {
+    //
+    super(name, position, salary);
+    this.#employeesManaged = [];
+    this.department = department;
+  }
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
+  setEmployeesManaged(employee) {
+    this.#employeesManaged.push(employee);
+  }
+  getDepartment() {
+    return this.department;
+  }
+  setDepartment(department) {
+    this.department = department;
+  }
 }
 
 module.exports = {
-    Manager,
-}
+  Manager,
+};
