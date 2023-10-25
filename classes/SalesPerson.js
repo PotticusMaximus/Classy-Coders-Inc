3a6559ee3bfe976cfe216f8cf5209b90ceedab19
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-25 11:23:14 +0100

    Sales person tests passed

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,21 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
-
-
-    
+class SalesPerson extends Employees {
+  #totalSales;
+  constructor(name, position, salary, clients) {
+    //
+    super(name, position, salary);
+    this.#totalSales = 0;
+    this.clients = [];
+  }
+  getSalesNumber() {
+    return this.#totalSales;
+  }
+  makeSale(amount) {
+    this.#totalSales += amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
